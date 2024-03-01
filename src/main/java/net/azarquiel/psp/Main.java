2d323539161606a6321b900acfadb0d8dc9046c0
Sufi <116170704+SufiAzarquiel@users.noreply.github.com>
2024-03-01 14:09:57 +0100

    Mandar mensaje a servidor si esta escuchando

--- src/main/java/net/azarquiel/psp/Main.java
+++ src/main/java/net/azarquiel/psp/Main.java
@@ -1,19 +1,30 @@
 package net.azarquiel.psp;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String Host="psp.sufiazarquiel.com";
+        int puerto=3030;
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // Crea socket si alguien escucha
+        Socket socketCliente = null;
 
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        // crear socket con try-with-resources
+        try (Socket socket = new Socket(Host, puerto)) {
+
+            socketCliente = socket;
+            PrintWriter out=new PrintWriter(socketCliente.getOutputStream());
+
+            // quitar delay para tener respuestas rapidas
+            socket.setTcpNoDelay(true);
+            // mandar mensaje
+            out.println("hola");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
