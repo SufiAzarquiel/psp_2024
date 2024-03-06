72409f38108927db652e49db2ac7841f755e827a
Sufi <116170704+SufiAzarquiel@users.noreply.github.com>
2024-03-06 09:12:28 +0100

    cambiar a lo de julio

--- src/main/java/net/azarquiel/psp/Main.java
+++ src/main/java/net/azarquiel/psp/Main.java
@@ -1,40 +1,17 @@
 package net.azarquiel.psp;
 
-
-import net.azarquiel.psp.backend.HiloCliente;
-import net.azarquiel.psp.backend.HiloServidor;
-import net.azarquiel.psp.backend.Servidor;
+import net.azarquiel.psp.game.Logica;
+import net.azarquiel.psp.game.Posicion;
 import net.azarquiel.psp.game.Tablero;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
-
 public class Main {
-    public static void main(String[] args) {
-        // variables de conexion
-        int puerto = 3030;
-        String host = "psp.sufiazarquiel.com";
-
-        // abrir la ventana de juego
-        Tablero tablero = new Tablero();
-
-        // conectarse al servidor
-        // recibir respuesta: emparejado o esperando
-        // si esperando, esperar a que se empareje
-        HiloCliente cliente = new HiloCliente(host, puerto, tablero);
-
-        // enviar el nombre del jugador
-        // si es tu turno:
-        // - cada vez que se haga un movimiento, enviarlo al servidor
-        // si no es tu turno:
-        // - esperar a que el oponente haga un movimiento
-        // recibir respuesta: ganador, empate o seguir jugando
-
-        // si emparejado, recibir:
-        // - el nombre del oponente
-        // - el turno (true o false) -> hilo
-        // - el tablero -> hilo
+	public static void main(final String[] args) {
+        final Posicion p = new Posicion();
+        final Tablero t = new Tablero(p);
+        final Logica juego = new Logica(t, p);
+        juego.Conecto();
+        final int turno = juego.turno();
+        p.cargaLetra((turno == 1) ? 'X' : 'O');
+        juego.inicio(turno);
     }
 }
