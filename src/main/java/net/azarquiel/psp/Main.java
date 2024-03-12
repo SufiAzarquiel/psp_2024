b5653fe16617cc2899dc3ea3f49d47e48a7adea0
Sufi <48214093+SufiDev@users.noreply.github.com>
2024-03-12 18:15:11 +0100

    cambiar algunos nombres

--- src/main/java/net/azarquiel/psp/Main.java
+++ src/main/java/net/azarquiel/psp/Main.java
@@ -8,10 +8,14 @@ public class Main {
 	public static void main(final String[] args) {
         final Posicion p = new Posicion();
         final Tablero t = new Tablero(p);
-        final Logica juego = new Logica(t, p);
-        juego.Conecto();
-        final int turno = juego.turno();
+        final Logica logica = new Logica(t, p);
+        logica.Conecto();
+        final int turno = logica.turno();
+
+        // leer nombre por consola
+        // enviar nombre al servidor
+        // recibir nombre del otro jugador y meterlo en el tablero
         p.cargaLetra((turno == 1) ? 'X' : 'O');
-        juego.inicio(turno);
+        logica.inicio(turno);
     }
 }
